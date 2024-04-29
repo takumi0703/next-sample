998de78f013b647e38aabf12ea95dbe5197f3b27
otani <otani@unbereal.co.jp>
2024-04-29 22:01:20 +0900

    link imageコンポーネント

--- pages/ssg.tsx
+++ pages/ssg.tsx
@@ -1,6 +1,8 @@
 
 import Head from "next/head"
 import { GetStaticProps, NextPage, NextPageContext } from "next"
+import Link from "next/link"
+import Image from "next/image"
 
 type SSGProps = {
   message: string
@@ -17,6 +19,13 @@ const SSG: NextPage<SSGProps> = (props) => {
       </Head>
       <div>
         <h1>SSGページだよ</h1>
+        {/* Linkコンポーネントを使って、リンクを作成 */}
+        {/* 必要なデータをあらかじめ非同期で取得しているため、aタグを利用するより高速 */}
+        <Link href="/ssr">
+          SSRに行きます
+        </Link>
+        {/* Imageも同様、サーバー側で読み込みが行われ、高速に表示できるらしい */}
+        <Image src="/next.svg" alt="Next.js Logo" width={200} height={300} />
         <p>{message}</p>
       </div>
     </>
